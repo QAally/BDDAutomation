ba1182207b1cf4191029b40962d5b65a7f33d018
Ally Atieah <aatieah@onehousing.co.uk>
2018-11-12 13:45:43 +0000

    Test Project Automation

---
+++ EATestProject/CrossPlatformEATest/Features/Logins.feature.cs
@@ -0,0 +1,35 @@
+﻿//#error Generation error: Message: Object reference not set to an instance of an object.
+
+//AppDomain Information: 
+//	Name: AppDomainForTestGeneration
+//	ConfigFile: C:\Development\EATestProject\packages\SpecFlow.2.3.2\tools\plugincompability.config
+//	BaseDirectory: C:\Development\EATestProject\packages\SpecFlow.2.3.2\tools
+
+//Loaded Assemblies:
+//Fullname | Location | Codebase
+//CommandLine, Version=2.3.0.0, Culture=neutral, PublicKeyToken=de6f01bd326f8c32 | c:\users\aatieah\appdata\local\microsoft\visualstudio\15.0_f5c85337\extensions\ttyfuqx3.bcr\CommandLine.dll | file:///c:/users/aatieah/appdata/local/microsoft/visualstudio/15.0_f5c85337/extensions/ttyfuqx3.bcr/CommandLine.DLL
+//Gherkin, Version=5.0.0.0, Culture=neutral, PublicKeyToken=86496cfa5b4a5851 | C:\Development\EATestProject\packages\SpecFlow.2.3.2\tools\Gherkin.dll | file:///C:/Development/EATestProject/packages/SpecFlow.2.3.2/tools/Gherkin.DLL
+//mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089 | C:\Windows\Microsoft.NET\Framework\v4.0.30319\mscorlib.dll | file:///C:/Windows/Microsoft.NET/Framework/v4.0.30319/mscorlib.dll
+//Newtonsoft.Json, Version=9.0.0.0, Culture=neutral, PublicKeyToken=30ad4fe6b2a6aeed | C:\Development\EATestProject\packages\SpecFlow.2.3.2\tools\Newtonsoft.Json.dll | file:///C:/Development/EATestProject/packages/SpecFlow.2.3.2/tools/Newtonsoft.Json.DLL
+//System, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089 | C:\Windows\Microsoft.Net\assembly\GAC_MSIL\System\v4.0_4.0.0.0__b77a5c561934e089\System.dll | file:///C:/Windows/Microsoft.Net/assembly/GAC_MSIL/System/v4.0_4.0.0.0__b77a5c561934e089/System.dll
+//System.Configuration, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a | C:\Windows\Microsoft.Net\assembly\GAC_MSIL\System.Configuration\v4.0_4.0.0.0__b03f5f7f11d50a3a\System.Configuration.dll | file:///C:/Windows/Microsoft.Net/assembly/GAC_MSIL/System.Configuration/v4.0_4.0.0.0__b03f5f7f11d50a3a/System.Configuration.dll
+//System.Core, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089 | C:\Windows\Microsoft.Net\assembly\GAC_MSIL\System.Core\v4.0_4.0.0.0__b77a5c561934e089\System.Core.dll | file:///C:/Windows/Microsoft.Net/assembly/GAC_MSIL/System.Core/v4.0_4.0.0.0__b77a5c561934e089/System.Core.dll
+//System.Data, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089 | C:\Windows\Microsoft.Net\assembly\GAC_32\System.Data\v4.0_4.0.0.0__b77a5c561934e089\System.Data.dll | file:///C:/Windows/Microsoft.Net/assembly/GAC_32/System.Data/v4.0_4.0.0.0__b77a5c561934e089/System.Data.dll
+//System.Numerics, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089 | C:\Windows\Microsoft.Net\assembly\GAC_MSIL\System.Numerics\v4.0_4.0.0.0__b77a5c561934e089\System.Numerics.dll | file:///C:/Windows/Microsoft.Net/assembly/GAC_MSIL/System.Numerics/v4.0_4.0.0.0__b77a5c561934e089/System.Numerics.dll
+//System.Runtime.Serialization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089 | C:\Windows\Microsoft.Net\assembly\GAC_MSIL\System.Runtime.Serialization\v4.0_4.0.0.0__b77a5c561934e089\System.Runtime.Serialization.dll | file:///C:/Windows/Microsoft.Net/assembly/GAC_MSIL/System.Runtime.Serialization/v4.0_4.0.0.0__b77a5c561934e089/System.Runtime.Serialization.dll
+//System.ValueTuple, Version=4.0.1.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51 | C:\Development\EATestProject\packages\SpecFlow.2.3.2\tools\System.ValueTuple.dll | file:///C:/Development/EATestProject/packages/SpecFlow.2.3.2/tools/System.ValueTuple.DLL
+//System.Xml, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089 | C:\Windows\Microsoft.Net\assembly\GAC_MSIL\System.Xml\v4.0_4.0.0.0__b77a5c561934e089\System.Xml.dll | file:///C:/Windows/Microsoft.Net/assembly/GAC_MSIL/System.Xml/v4.0_4.0.0.0__b77a5c561934e089/System.Xml.dll
+//System.Xml.Linq, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089 | C:\Windows\Microsoft.Net\assembly\GAC_MSIL\System.Xml.Linq\v4.0_4.0.0.0__b77a5c561934e089\System.Xml.Linq.dll | file:///C:/Windows/Microsoft.Net/assembly/GAC_MSIL/System.Xml.Linq/v4.0_4.0.0.0__b77a5c561934e089/System.Xml.Linq.dll
+//TechTalk.SpecFlow, Version=2.3.2.0, Culture=neutral, PublicKeyToken=0778194805d6db41 | C:\Development\EATestProject\packages\SpecFlow.2.3.2\tools\TechTalk.SpecFlow.dll | file:///C:/Development/EATestProject/packages/SpecFlow.2.3.2/tools/TechTalk.SpecFlow.DLL
+//TechTalk.SpecFlow.Generator, Version=2.3.2.0, Culture=neutral, PublicKeyToken=0778194805d6db41 | C:\Development\EATestProject\packages\SpecFlow.2.3.2\tools\TechTalk.SpecFlow.Generator.dll | file:///C:/Development/EATestProject/packages/SpecFlow.2.3.2/tools/TechTalk.SpecFlow.Generator.DLL
+//TechTalk.SpecFlow.Parser, Version=2.3.2.0, Culture=neutral, PublicKeyToken=0778194805d6db41 | C:\Development\EATestProject\packages\SpecFlow.2.3.2\tools\TechTalk.SpecFlow.Parser.dll | file:///C:/Development/EATestProject/packages/SpecFlow.2.3.2/tools/TechTalk.SpecFlow.Parser.DLL
+//TechTalk.SpecFlow.Reporting, Version=2.3.2.0, Culture=neutral, PublicKeyToken=0778194805d6db41 | C:\Development\EATestProject\packages\SpecFlow.2.3.2\tools\TechTalk.SpecFlow.Reporting.dll | file:///C:/Development/EATestProject/packages/SpecFlow.2.3.2/tools/TechTalk.SpecFlow.Reporting.DLL
+//TechTalk.SpecFlow.Utils, Version=2.3.2.0, Culture=neutral, PublicKeyToken=0778194805d6db41 | C:\Development\EATestProject\packages\SpecFlow.2.3.2\tools\TechTalk.SpecFlow.Utils.dll | file:///C:/Development/EATestProject/packages/SpecFlow.2.3.2/tools/TechTalk.SpecFlow.Utils.DLL
+//TechTalk.SpecFlow.VisualStudio.CodeBehindGenerator, Version=2017.2.7.0, Culture=neutral, PublicKeyToken=0778194805d6db41 | c:\users\aatieah\appdata\local\microsoft\visualstudio\15.0_f5c85337\extensions\ttyfuqx3.bcr\TechTalk.SpecFlow.VisualStudio.CodeBehindGenerator.exe | file:///c:/users/aatieah/appdata/local/microsoft/visualstudio/15.0_f5c85337/extensions/ttyfuqx3.bcr/TechTalk.SpecFlow.VisualStudio.CodeBehindGenerator.exe
+
+//System.NullReferenceException: Object reference not set to an instance of an object.
+//   at TechTalk.SpecFlow.Generator.UnitTestFeatureGenerator.GenerateUnitTestFixture(SpecFlowDocument document, String testClassName, String targetNamespace)
+//   at TechTalk.SpecFlow.Generator.TestGenerator.GenerateTestFileCode(FeatureFileInput featureFileInput)
+//   at TechTalk.SpecFlow.Generator.TestGenerator.GetGeneratedTestCode(FeatureFileInput featureFileInput)
+//   at TechTalk.SpecFlow.Generator.TestGenerator.GenerateTestFileWithExceptions(FeatureFileInput featureFileInput, GenerationSettings settings)
+//   at TechTalk.SpecFlow.Generator.ErrorHandlingTestGenerator.GenerateTestFile(FeatureFileInput featureFileInput, GenerationSettings settings)
